e7c677b0c1395388decbf18397320694d5981750
Antonio José Barrios León <ajosebarrios@gmail.com>
2017-01-21 20:15:34 +0100

    refactor(*): visual refactor

--- index.js
+++ index.js
@@ -1,25 +1,16 @@
 // Dependencies
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
 
-var connectedUsers = [];
+// Routes
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+// sockets events
+io.on('connection', (socket) => {
+    socket.on('ping message', (msg) => socket.emit('pong message', msg));
 });
 
-io.on('connection', function(socket) {
-    socket.on('chat message', function(msg) {
-        socket.emit('chat message', { from: 'server', timestamp: Date.now(), content: msg.content });
-    });
-
-    socket.on('nick message', function(msg) {
-        console.log(msg + ' has set its nickname!');
-    });
-});
-
-http.listen(port, function() {
-    console.log('listening on: ' + port);
-});
+// Start
+http.listen(port, () => console.log('Server listening on: ' + port));
