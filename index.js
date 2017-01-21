31e4e97ee0b15a64223e2db43240c6d13656386e
Antonio José Barrios León <ajosebarrios@gmail.com>
2017-01-21 17:21:16 +0100

    fix(port): change to dynamic random node port

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var port = process.env.port || 3000;
 
 var connectedUsers = [];
 
@@ -19,6 +20,6 @@ io.on('connection', function(socket) {
     });
 });
 
-http.listen(process.env.port || 3000, function() {
-    console.log('listening on *:3000');
+http.listen(port, function() {
+    console.log('listening on: ' + port);
 });
